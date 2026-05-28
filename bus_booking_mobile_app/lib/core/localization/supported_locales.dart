07e34e20d3cd9f7105c10b3cd1fe445aa15a0e1b
Thiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2026-05-29 02:31:45 +0530

    feat: definde multiple language feature

---
+++ bus_booking_mobile_app/lib/core/localization/supported_locales.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class SupportedLocales {
+  static const List<Locale> supportedLocales = [
+    Locale('en', 'US'), // English (United States)
+    Locale('si', 'LK'), // Sinhala (Sri Lanka
+    Locale('ta', 'LK'), // Tamil (Sri Lanka)
+  ];
+}
