d65e3fff3f16bbccd34e4afb0499cc4ddbc911ed
Thiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2026-05-29 18:13:19 +0530

    feat: add multiprovider and set up language controller

--- bus_booking_mobile_app/lib/main.dart
+++ bus_booking_mobile_app/lib/main.dart
@@ -2,13 +2,35 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'core/localization/language_controller.dart';
 import 'routes/app_routes.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import "../../../core/localization/l10n/app_localizations.dart";
+import 'package:google_fonts/google_fonts.dart';
+import 'features/login/auth_data.dart';
+import 'features/login/auth_repository.dart';
+import 'features/login/auth_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(ChangeNotifierProvider(
-      create: (_) => LanguageController()..loadSavedLanguage(),
-      child:const MyApp()
-  ));
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) =>
+              LanguageController()..loadSavedLanguage(),
+        ),
+
+        ChangeNotifierProvider(
+          create: (_) => AuthProvider(
+            AuthRepository(
+              AuthData(),
+            ),
+          ),
+        ),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -16,9 +38,32 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      initialRoute: '/',
-      routes: AppRoutes.routes,
+    return Consumer<LanguageController>(
+      builder: (context, lang, child) {
+        return MaterialApp(
+          locale: lang.currentLocale,
+
+          theme: ThemeData(
+            textTheme: GoogleFonts.notoSansTextTheme(),
+          ),
+
+          supportedLocales: const [
+            Locale('en'),
+            Locale('si'),
+            Locale('ta'),
+          ],
+
+          localizationsDelegates: const [
+            AppLocalizations.delegate,
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+
+          initialRoute: '/',
+          routes: AppRoutes.routes,
+        );
+      },
     );
   }
 }
