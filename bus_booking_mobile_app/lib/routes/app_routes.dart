b8b134228a543310f3a535aab50fdb6160f5bce3
Thiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2026-05-29 18:10:46 +0530

    feat: add app route

--- bus_booking_mobile_app/lib/routes/app_routes.dart
+++ bus_booking_mobile_app/lib/routes/app_routes.dart
@@ -1,12 +1,13 @@
-import 'package:flutter/material.dart';
 import '../features/introduction/presentation/introduction_screen.dart';
 import '../features/login/presentation/login_screen.dart';
 import '../core/app/splash_screen.dart';
+import '../features/home/presentation/home_screen.dart';
 
 class AppRoutes {
   static final routes = {
     '/': (context) => const SplashScreen(),
     '/intro': (context) => const IntroductionScreen(),
     '/login': (context) => const LoginScreen(),
+    '/home': (context) => const HomeScreen(),
   };
 }
